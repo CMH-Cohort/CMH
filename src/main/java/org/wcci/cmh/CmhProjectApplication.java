999e60698d6857a49641742a829c9ca10012ad67
Jed Duffey <jedduffey@gmail.com>
2016-12-08 10:33:03 -0500

    adding gradle files and spring

---
+++ src/main/java/org/wcci/cmh/CmhProjectApplication.java
@@ -0,0 +1,12 @@
+package org.wcci.cmh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmhProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmhProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/org/wcci/cmh/CmhProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wcci.cmh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CmhProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
