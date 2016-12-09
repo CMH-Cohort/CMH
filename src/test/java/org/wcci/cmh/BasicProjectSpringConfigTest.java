775a98fa4c49144d991323f8d88bf5ed7b500d52
We Can Code IT <you@example.com>
2016-12-09 11:39:15 -0500

    made basic spring config test

---
+++ src/test/java/org/wcci/cmh/BasicProjectSpringConfigTest.java
@@ -0,0 +1,17 @@
+package org.wcci.cmh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class BasicProjectSpringConfigTest {
+
+	  @Test
+	  public void shouldLoadConfiguration() {
+	    // don't really need to do anything -- we're just checking for an initialization failure
+	  }
+	}
+
