063f365486113f82097479d8a502658c33c7cc3d
Jed Duffey <jedduffey@gmail.com>
2016-12-12 13:35:30 -0500

    adding more code

--- src/main/java/org/wcci/cmh/TermController.java
+++ src/main/java/org/wcci/cmh/TermController.java
@@ -1,5 +1,7 @@
 package org.wcci.cmh;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,37 +12,42 @@ import org.wcci.cmh.Term;
 @Controller
 public class TermController {
 
-    @RequestMapping("/term-list")
-    public String displayEntireListOfTerms(String name, Model model) {
-        return "term-list";
-    }
-    
-    @RequestMapping("/term-single")
-    public String displayASingleTerm(@RequestParam(value="name", required=false) String name, Model model) {
-        return "term-single";
-    }
-    
-//	@RequestMapping("/review")
-//	public String displayReview(@RequestParam("id") long id, Model model) {
-//		Review review = myReviewRepository.findOne(id);
-//		model.addAttribute("selectedReview", review);
-//		return "single-review";
-//	}
-//	
-//	@RequestMapping("/all")
-//	public String displayAll(Model model) {
-//		Iterable<Review> reviews = myReviewRepository.findAll();
-//		model.addAttribute("reviews", reviews);
-//		Iterable<Category> categories = myCategoryRepository.findAll();
-//		model.addAttribute("categories", categories);
-//		return "all-reviews";
-//	}
-//	
-//	@RequestMapping("/categories")
-//	public String displayCategories(Model model) {
-//		Iterable<Category> categories = myCategoryRepository.findAll();
-//		model.addAttribute("categories", categories);
-//		return "all-categories";
-//	}
+	@Resource
+	private TermRepository myTermRepository;
+
+	@RequestMapping("/term-list")
+	public String displayEntireListOfTerms(String name, Model model) {
+		return "term-list";
+	}
+
+	@RequestMapping("/term-single")
+	public String displayASingleTerm(@RequestParam(value = "name", required = false) long id, Model model) {
+		Term term = myTermRepository.findOne(id);
+		model.addAttribute("selectedTerm", term);
+		return "term-single";
+	}
+
+	// @RequestMapping("/review")
+	// public String displayReview(@RequestParam("id") long id, Model model) {
+	// Review review = myReviewRepository.findOne(id);
+	// model.addAttribute("selectedReview", review);
+	// return "single-review";
+	// }
+	//
+	// @RequestMapping("/all")
+	// public String displayAll(Model model) {
+	// Iterable<Review> reviews = myReviewRepository.findAll();
+	// model.addAttribute("reviews", reviews);
+	// Iterable<Category> categories = myCategoryRepository.findAll();
+	// model.addAttribute("categories", categories);
+	// return "all-reviews";
+	// }
+	//
+	// @RequestMapping("/categories")
+	// public String displayCategories(Model model) {
+	// Iterable<Category> categories = myCategoryRepository.findAll();
+	// model.addAttribute("categories", categories);
+	// return "all-categories";
+	// }
 
 }
