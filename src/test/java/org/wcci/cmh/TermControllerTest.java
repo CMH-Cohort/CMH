2972b332e6467ea81c92e408c3990ac37770e14f
Jed Duffey <jedduffey@gmail.com>
2016-12-12 11:15:32 -0500

    adding imports

--- src/test/java/org/wcci/cmh/TermControllerTest.java
+++ src/test/java/org/wcci/cmh/TermControllerTest.java
@@ -1,5 +1,16 @@
 package org.wcci.cmh;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class TermControllerTest {
+
+	@Test
+	public void firstTest() {
+	}
 
 }
