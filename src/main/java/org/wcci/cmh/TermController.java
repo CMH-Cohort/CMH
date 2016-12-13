2fee80e240092b675f42dd156f528797088a9031
Seth <you@example.com>
2016-12-13 15:22:06 -0500

    search

--- src/main/java/org/wcci/cmh/TermController.java
+++ src/main/java/org/wcci/cmh/TermController.java
@@ -13,19 +13,28 @@ public class TermController {
 	@Resource
 	private TermRepository myTermRepository;
 
-	@RequestMapping("/term-list")
+	@RequestMapping("/all")
 	public String displayEntireListOfTerms(Model model) {
 	
 		Iterable<Term> terms = myTermRepository.findAll();
 		model.addAttribute("terms", terms);
 		return "term-list";
 	}
+	
+	
 
 	@RequestMapping("/term-single")
 	public String displayASingleTerm(@RequestParam(value = "name", required = false) long id, Model model) {
 		Term term = myTermRepository.findOne(id);
 		model.addAttribute("selectedTerm", term);
 		return "term-single";
+	}
+
+	@RequestMapping("/search")
+	public String search(@RequestParam(value = "searchTitle") String title, Model model) {
+		Iterable<Term> searchResults = myTermRepository.findByTitleLike("%" + title + "%");
+		model.addAttribute("terms", searchResults);
+		return "term-list";
 	}
 	
 
--- src/test/java/org/wcci/cmh/TermControllerTest.java
+++ src/test/java/org/wcci/cmh/TermControllerTest.java
@@ -1,24 +1,54 @@
 package org.wcci.cmh;
 
-import javax.annotation.Resource;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.Collection;
+
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
+import junit.framework.Assert;
+
 public class TermControllerTest {
 
-	@Resource
+	@InjectMocks
 	private TermController underTest;
+
+	@Mock
+	private TermRepository repository;
+
+	@Mock
+	private Collection<Term> searchResults;
+	
+	@Mock
+	private Model model;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
 	
 	@Test
-	public void shouldFindAllTerms() {
-		
-//		underTest.displayEntireListOfTerms(name, model);
-		
+	public void shouldSearch() {
+		when(repository.findByTitleLike("%Nonsense%")).thenReturn(searchResults);
+
+		underTest.search("Nonsense", model);
+
+		verify(model).addAttribute("terms",searchResults);
 	}
+	
+	@Test
+	public void shouldReturnSearchPage() {
+		String result = underTest.search("NoMatter", model);
+				
+		Assert.assertEquals("term-list", result);
+	}
+	
+
 
 }
