80727929d9f25e81042d24e8d560dde4bf0b7e5b
Jed Duffey <jedduffey@gmail.com>
2016-12-15 15:20:48 -0500

    modifying code from copy-paste to apply to user class

--- src/main/java/org/wcci/cmh/UserRepository.java
+++ src/main/java/org/wcci/cmh/UserRepository.java
@@ -1,5 +1,18 @@
 package org.wcci.cmh;
 
-public class UserRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Collection;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    // This method allows us to search the 'title' field of the users.
+    Collection<User> findByTitleLike(String title);
+
+    // This method allows us to do a case-insensitive search of the 'title' field of the users.
+    Collection<User> findByTitleIgnoreCaseLike(String title);
+
+    // This method allows us to do a case-insensitive selection of 'title' field of users.
+    User findByTitleIgnoreCase(String title);
 
 }
--- src/test/java/org/wcci/cmh/UserRepositoryJpaTest.java
+++ src/test/java/org/wcci/cmh/UserRepositoryJpaTest.java
@@ -1,9 +1,17 @@
 package org.wcci.cmh;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @DataJpaTest
-@RunWith(SpringRunner.class
+@RunWith(SpringRunner.class)
 public class UserRepositoryJpaTest {
 	
 	@Resource
