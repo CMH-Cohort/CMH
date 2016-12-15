a3c17d756d0e827340b0fbef5d299032f3eb1dcf
Jed Duffey <jedduffey@gmail.com>
2016-12-15 14:28:26 -0500

    removed password field from user class

--- src/main/java/org/wcci/cmh/User.java
+++ src/main/java/org/wcci/cmh/User.java
@@ -11,11 +11,6 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	private String username;
-	private String password;
-	
-	
-	
-	
+	private String username;	
 
 }
