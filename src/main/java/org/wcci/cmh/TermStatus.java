4b946228fcdda41a4ea977c67614a92810861726
B. T. Forsythe <brian@btforsythe.io>
2016-12-16 14:12:30 -0500

    added starter TermStatusJpaTest

---
+++ src/main/java/org/wcci/cmh/TermStatus.java
@@ -0,0 +1,12 @@
+package org.wcci.cmh;
+
+import javax.persistence.Entity;
+
+@Entity
+public class TermStatus {
+
+	public Term getTerm() {
+		return null;
+	}
+
+}
---
+++ src/main/java/org/wcci/cmh/TermStatusRepository.java
@@ -0,0 +1,7 @@
+package org.wcci.cmh;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TermStatusRepository extends CrudRepository<TermStatus, Long> {
+
+}
